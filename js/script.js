9df37b72367d18df4f15d95a2e88db914d96cd6c
Iago Machado <83992018+iagomachadoo@users.noreply.github.com>
2021-10-21 18:02:30 -0300

    Criando arrays para receber os valores das notas e dos pesos
    Criei laços for para adicionar quantos label e input forem necessários com base no número de notas e adicionar os valores desses dentro de arrays. Depois foi criado outro laço for para somar os valores dessas arays e calcular a média e média ponderada.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let inputQuantidadeAvaliacoes = document.getElementById('quantidadeDeAvaliacoes');
+const inputQuantidadeAvaliacoes = document.getElementById('quantidadeDeAvaliacoes');
 
 let inputRadioMediaMediaPonderada = document.getElementsByName('media-ou-media-ponderada');
 
@@ -8,17 +8,29 @@ let btnCalcular = document.getElementById('btn-calcular');
 
 let divResultado = document.getElementById('resultado');
 
-let listaQuantidadeAvaliacoes = [];
+let elementoBtnCalcular = document.createElement('input');
 
-function inserirNotas() { 
+elementoBtnCalcular.setAttribute('type', 'button');
 
+elementoBtnCalcular.setAttribute('onclick', 'calcular()');
+
+let listaNotas = [];
+
+let listaPesos = [];
+
+function inserirNotas() {
+    
+    divResultado.innerHTML = '';
+    
     if (inputRadioMediaMediaPonderada[0].checked){
-        
+
+        divNotas.innerHTML = '';
+
         for(let i = 1; i <= inputQuantidadeAvaliacoes.value; i++){
             
             let elementoLabel = document.createElement('label');
 
-            elementoLabel.setAttribute('id', `label-nota${i}`);
+            elementoLabel.setAttribute('for', `input-nota${i}`);
 
             elementoLabel.textContent = `Avaliação ${i}`;
 
@@ -34,19 +46,21 @@ function inserirNotas() {
             
         }
 
-        let elementoBtnCalcular = document.createElement('input');
-        elementoBtnCalcular.setAttribute('type', 'button');
-        elementoBtnCalcular.setAttribute('value', 'Calcular Média');
-        btnCalcular.appendChild(elementoBtnCalcular);
-
+        btnCalcular.innerHTML = '';
         
+        elementoBtnCalcular.setAttribute('value', 'Calcular Média');
+        
+        btnCalcular.appendChild(elementoBtnCalcular);
+ 
     }else if(inputRadioMediaMediaPonderada[1].checked){
 
+        divNotas.innerHTML = ''
+
         for(let i = 1; i <= inputQuantidadeAvaliacoes.value; i++){
             
             let elementoLabel = document.createElement('label');
 
-            elementoLabel.setAttribute('id', `label-nota${i}`);
+            elementoLabel.setAttribute('for', `input-nota${i}`);
 
             elementoLabel.textContent = `Avaliação ${i}`;
 
@@ -56,15 +70,32 @@ function inserirNotas() {
 
             elementoInput.setAttribute('id', `input-nota${i}`);
 
+            let elementoLabelPeso = document.createElement('label');
+
+            elementoLabelPeso.setAttribute('for', `input-peso${i}`);
+
+            elementoLabelPeso.textContent = 'Peso';
+
+            let elementoInputPeso = document.createElement('input');
+
+            elementoInputPeso.setAttribute('type', 'number');
+
+            elementoInputPeso.setAttribute('id', `input-peso${i}`);
+
             divNotas.appendChild(elementoLabel);
 
             divNotas.appendChild(elementoInput);
+
+            divNotas.appendChild(elementoLabelPeso);
+
+            divNotas.appendChild(elementoInputPeso);
             
         }
 
-        let elementoBtnCalcular = document.createElement('input');
-        elementoBtnCalcular.setAttribute('type', 'button');
+        btnCalcular.innerHTML = '';
+
         elementoBtnCalcular.setAttribute('value', 'Calcular Média Ponderada');
+
         btnCalcular.appendChild(elementoBtnCalcular);
 
     }else{
@@ -76,243 +107,57 @@ function inserirNotas() {
         divResultado.appendChild(elementoParagrafo);
     }
 }
+
 function calcular() {
+
+    let somaNotas = 0;/*inicializando a soma das notas*/
+
+    let somaPesos = 0;/*inicializando a soma dos pesos*/
+
+    let produtoNotaPeso = 0;
+
+    let media = 0;/*inicializando a media*/
+
+    let mediaPonderada = 0;
+
+    listaNotas.length = 0;/*zerando a array*/
+
+    listaPesos.length = 0;/*zerando a array*/
+
+    for(let j = 1; j <= inputQuantidadeAvaliacoes.value; j++){
+
+        let valorNotas = document.getElementById(`input-nota${j}`);
+
+        let valorPesos = document.getElementById(`input-peso${j}`);
+
+        listaNotas.push(Number(valorNotas.value));
+
+        listaPesos.push(Number(valorPesos.value));
+        
+    }/*laço para pegar os inputs que vão receber as notas e jogar dentro da array listaNotas. Vai realizar o mesmo procedimento para os pesos*/
     
-    let quantidadeDeAvaliacoes = Number(document.getElementById('quantidadeDeAvaliacoes').value);
-
-    let mediaOuMediaPonderada = document.getElementsByName('media-ou-media-ponderada')
-
-    if (quantidadeDeAvaliacoes == 2 && mediaOuMediaPonderada[0].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let resultadoDaMedia = (nota1 + nota2) / quantidadeDeAvaliacoes
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '300px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-        
-    }else if (quantidadeDeAvaliacoes == 3 && mediaOuMediaPonderada[0].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let nota3 = Number(document.querySelector('#nota3').value);
-
-        let resultadoDaMedia = (nota1 + nota2 + nota3) / quantidadeDeAvaliacoes
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '300px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-        
-    }else if (quantidadeDeAvaliacoes == 4 && mediaOuMediaPonderada[0].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let nota3 = Number(document.querySelector('#nota3').value);
-
-        let nota4 = Number(document.querySelector('#nota4').value);
-
-        let resultadoDaMedia = (nota1 + nota2 + nota3 + nota4) / quantidadeDeAvaliacoes
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '300px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-        
-    }else if (quantidadeDeAvaliacoes == 5 && mediaOuMediaPonderada[0].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let nota3 = Number(document.querySelector('#nota3').value);
-
-        let nota4 = Number(document.querySelector('#nota4').value);
-
-        let nota5 = Number(document.querySelector('#nota5').value);
-
-        let resultadoDaMedia = (nota1 + nota2 + nota3 + nota4 + nota5) / quantidadeDeAvaliacoes
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '300px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-        
-    }else if (quantidadeDeAvaliacoes == 2 && mediaOuMediaPonderada[1].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let peso1 = Number(document.querySelector('#peso1').value);
-
-        let peso2 = Number(document.querySelector('#peso2').value);
-
-        let somaDosPesos = peso1 + peso2
-
-        let resultadoDaMedia = ((nota1 * peso1) + (nota2 * peso2)) / somaDosPesos
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média Ponderada: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '450px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-
-    }else if (quantidadeDeAvaliacoes == 3 && mediaOuMediaPonderada[1].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let nota3 = Number(document.querySelector('#nota3').value);
-
-        let peso1 = Number(document.querySelector('#peso1').value);
-
-        let peso2 = Number(document.querySelector('#peso2').value);
-
-        let peso3 = Number(document.querySelector('#peso3').value);
-
-        let somaDosPesos = peso1 + peso2 + peso3
-
-        let resultadoDaMedia = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / somaDosPesos
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média Ponderada: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '450px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-
-    }else if (quantidadeDeAvaliacoes == 4 && mediaOuMediaPonderada[1].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let nota3 = Number(document.querySelector('#nota3').value);
-
-        let nota4 = Number(document.querySelector('#nota4').value);
-
-        let peso1 = Number(document.querySelector('#peso1').value);
-
-        let peso2 = Number(document.querySelector('#peso2').value);
-
-        let peso3 = Number(document.querySelector('#peso3').value);
-
-        let peso4 = Number(document.querySelector('#peso4').value);
-
-        let somaDosPesos = peso1 + peso2 + peso3 + peso4
-
-        let resultadoDaMedia = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4)) / somaDosPesos
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média Ponderada: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '450px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-
-    }else if (quantidadeDeAvaliacoes == 5 && mediaOuMediaPonderada[1].checked) {
-        let nota1 = Number(document.querySelector('#nota1').value);
-
-        let nota2 = Number(document.querySelector('#nota2').value);
-
-        let nota3 = Number(document.querySelector('#nota3').value);
-
-        let nota4 = Number(document.querySelector('#nota4').value);
-
-        let nota5 = Number(document.querySelector('#nota5').value);
-
-        let peso1 = Number(document.querySelector('#peso1').value);
-
-        let peso2 = Number(document.querySelector('#peso2').value);
-
-        let peso3 = Number(document.querySelector('#peso3').value);
-
-        let peso4 = Number(document.querySelector('#peso4').value);
-
-        let peso5 = Number(document.querySelector('#peso5').value);
-
-        let somaDosPesos = peso1 + peso2 + peso3 + peso4 + peso5
-
-        let resultadoDaMedia = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3) + (nota4 * peso4) + (nota5 * peso5)) / somaDosPesos
-
-        let divResultado = document.getElementById('resultado');
-
-        divResultado.innerHTML = `<p>Média Ponderada: ${resultadoDaMedia.toFixed(2)}</p>`
-
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '450px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
-
-    }else{
-        let divResultado = document.getElementById('resultado');
-    
-        divResultado.innerHTML = `<p>Erro, preencher os campos.</p>`
-        divResultado.style.background= 'var(--cor1)'
-        divResultado.style.width= '300px'
-        divResultado.style.height= '50px'
-        divResultado.style.margin= 'auto'
-        divResultado.style.display= 'flex'
-        divResultado.style.borderRadius= '10px'
-        divResultado.style.border= '1px solid black'
-        divResultado.style.boxShadow= '5px 2px 7px rgba(0, 0, 0, 0.300)'
+    for(let x in listaNotas){
+        somaNotas += listaNotas[x]
+    }/*laço que vai percorrer a array listaNotas e somar todos os seus valores*/
+
+    for(let y in listaPesos){
+        somaPesos += listaPesos[y]
+    }/*laço que vai percorrer a array listaPesos e somar todos os seus valores*/
+
+    for (let z in listaNotas, listaPesos){
+        produtoNotaPeso += listaNotas[z] * listaPesos[z]
     }
+    
+    media = somaNotas / listaNotas.length;
+
+    mediaPonderada = produtoNotaPeso / somaPesos;
+
+    /*
+    console.log(listaNotas);
+    console.log(listaPesos);
+    console.log(somaNotas);
+    console.log(somaPesos);
+    */
+    console.log(media);
+    console.log(mediaPonderada);
 }
